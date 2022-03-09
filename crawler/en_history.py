96e5780143d69af558a711061b453cca59d80367
Mi-Gyeong <51354302+rmfosem613@users.noreply.github.com>
2022-03-09 16:34:23 +0900

    Update en_history.py

--- crawler/en_history.py
+++ crawler/en_history.py
@@ -43,5 +43,5 @@ def ukraineHistory_en(search, page, file_name, category):
 
 
 # English
-ukraineHistory_en("ukraine", 2, "English_UkraineSupportNewsData", "history")
+ukraineHistory_en("ukraine", 2, "English_UkraineHistory", "history")
 print(english_result)
