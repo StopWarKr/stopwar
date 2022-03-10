57463bb7ff967aa6f4a6aa88ecadb35ee796ab27
beeo-wing <beeo.wing@kakaocorp.com>
2022-03-10 10:04:10 +0900

    add json file category tag

--- crawler/sponser.py
+++ crawler/sponser.py
@@ -8,18 +8,18 @@ english_result = []
 korean_result = []
 
 def createJs(title, result): # json 파일로 변경
-  with open('{}.json'.format(title), 'w', encoding = "UTF-8-sig") as f_write:
+  with open('{}.json'.format('./crawlingData/' + title), 'w', encoding = "UTF-8-sig") as f_write:
     json.dump(result, f_write, ensure_ascii = False, indent = 4)
 
   data = ""
-  with open('{}.json'.format(title), "r", encoding = "UTF-8-sig") as f:
+  with open('{}.json'.format('./crawlingData/' + title), "r", encoding = "UTF-8-sig") as f:
     line = f.readline()
     while line:
       data += line
       line = f.readline()
 
 
-def google_support(search, page, file_name):
+def google_sponser(search, page, file_name, category):
     start = 0
     for i in range(1, page + 1):
         url = 'https://www.google.com/search?q={}&hl=ko&tbm=nws&ei=4eMlYunIJNyUr7wPnrm0oAo&start={}&sa=N&ved=2ahUKEwipvcTD6rP2AhVcyosBHZ4cDaQ4KBDy0wN6BAgBED8&biw=1920&bih=937&dpr=1'.format(search, start)
@@ -32,19 +32,17 @@ def google_support(search, page, file_name):
         for i in range(len(news)):
             title = news[i].find("div", attrs = {"class" : "mCBkyc y355M JQe2Ld nDgy9d"}).get_text()
             link = news[i].find("a", {"class" : "WlydOe"})["href"]
-            if search == 'Ukraine Sponsorship':
-                english_result.append({'제목': title, '링크': link})
+            if search == 'Ukraine Sponsor':
+                english_result.append({'제목': title, '링크': link, '카테고리': category})
             else:
-                korean_result.append({'제목': title, '링크': link})
-        if search == 'Ukraine Sponsorship':
+                korean_result.append({'제목': title, '링크': link, '카테고리': category})
+        if search == 'Ukraine Sponsor':
             createJs(file_name, english_result)
         else:
             createJs(file_name, korean_result)
 
 # English
-google_support('Ukraine Sponsorship', 3, 'English_UkraineSupportNewsData')
-print(english_result)
+google_sponser('Ukraine Sponsor', 3, 'en_SponserNewsData', '후원')
 
 # Korean
-google_support('우크라이나 후원', 3, 'Korean_UkraineSupportNewsData')
-print(korean_result)
+google_sponser('우크라이나 후원', 3, 'kr_SponserNewsData', '후원')
