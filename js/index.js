1d8113156115e74d6b3018ba9b00ec0e2c1931cd
chango <kimtothechang@gmail.com>
2022-03-12 23:58:32 +0900

    :sparkles: 더보기 기능 구현

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
-import { getNews } from './modules/category.js';
+import { getNews, initialRenderCard, renderTen } from './modules/category.js';
 import { search } from './modules/search.js';
 
+let data = await getNews('ALL');
+
 // 최초 ALL 카테고리 렌더링
-window.onload = getNews('ALL');
+// window.onload = getNews('ALL');
 
 // 카테고리 클릭 이벤트(이벤트 캡쳐링)
 const buttonWrapper = document.querySelector('.news-category');
 
-buttonWrapper.addEventListener('click', (e) => {
+buttonWrapper.addEventListener('click', async (e) => {
   const btnNodes = [...buttonWrapper.children];
 
   btnNodes.forEach((node) => {
     node.classList.remove('on');
   });
 
-  // for (let btn in buttonWrapper.children)
-
   const currentNode = e.target;
   if (currentNode.nodeName === 'BUTTON') {
     currentNode.parentElement.classList.add('on');
-    getNews(currentNode.textContent);
+    initialRenderCard();
+    console.log(data);
+    data = await getNews(currentNode.textContent);
   }
 });
 
@@ -43,10 +45,17 @@ export const closeModal = () => {
 // openBtn.addEventListener('click', openModal);
 // closeBtn.addEventListener('click', closeModal);
 
-// [이예슬] modal background click -> modal close
-document.addEventListener('mousemove', (e) => {
-  const element = document.elementFromPoint(e.pageX, e.pageY);
-  const isBg = element.className == 'modal-background';
+// // [이예슬] modal background click -> modal close
+// document.addEventListener('mousemove', (e) => {
+//   const element = document.elementFromPoint(e.pageX, e.pageY);
+//   const isBg = element.className == 'modal-background';
 
-  if (isBg) closeBtn();
+//   if (isBg) closeBtn();
+// });
+
+// 더보기 클릭 시 10가 카드 추가 렌더링
+const btnMore = document.querySelector('.button-more');
+
+btnMore.addEventListener('click', (e) => {
+  renderTen(data);
 });
