90dbf89752802fe84e57c223ae06b8163958f4e3
chango <kimtothechang@gmail.com>
2022-03-15 01:26:36 +0900

    ✨ 구글 번역 API 기능 추가

--- js/index.js
+++ js/index.js
@@ -59,3 +59,19 @@ const btnMore = document.querySelector('.button-more');
 btnMore.addEventListener('click', (e) => {
   renderTen(data);
 });
+
+// 구글 번역 API 스타일링 => css로 할 경우 가끔씩 안되는 문제, 시간차 때문인가..
+const translateBox = document.querySelector('#google_translate_element');
+const translateIcon = document.querySelector('.goog-te-gadget-icon');
+const translateBoxStyle = document.querySelector('.goog-te-gadget-simple');
+const translateKorea = document.querySelector('.goog-te-menu-value');
+
+translateBox.style.overflow = 'hidden';
+translateBoxStyle.style.padding = '4px';
+translateBoxStyle.style.width = '75px';
+
+translateKorea.textContent = '한국어';
+
+translateIcon.style.backgroundImage = 'url("./image/icon-lang.svg")';
+translateIcon.style.backgroundPosition = 'center';
+translateIcon.style.backgroundRepeat = 'no-repeat';
