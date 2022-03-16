42ce6e2d3824c7e4ba7848d6afa17c7e5a9ece78
paullabkorea <paullabkorea@gmail.com>
2022-03-16 16:25:35 +0900

    크롤링 데이터 push

--- crawler/total_news_crawler.py
+++ crawler/total_news_crawler.py
@@ -12,7 +12,8 @@ from common.en_HistoryNewsData import ukraineHistory_en
 try:
     GoogleCrawler('교전 최신 상황').crawl_news('우크라이나 교전 상황', 2).write_json('kr_BattleNewsData')
     print('kr_BattleNewsData')
-except:
+except Exception as e:
+    print(e)
     print('kr_BattleNewsData error')
 
 # TODO :이 사이트만 크롤링이 타 사이트인데 수정 여부 논의 필요
@@ -22,11 +23,11 @@ except:
 # except:
 #     print('en_HistoryNewsData error')
 
-# try:
-#     GoogleCrawler('역사').crawl_news('우크라이나 역사', 2).write_json('kr_HistoryNewsData')
-#     print('kr_HistoryNewsData')
-# except:
-#     print('kr_HistoryNewsData error')
+try:
+    GoogleCrawler('역사').crawl_news('우크라이나 역사', 2).write_json('kr_HistoryNewsData')
+    print('kr_HistoryNewsData')
+except:
+    print('kr_HistoryNewsData error')
 
 # try:
 #     GoogleCrawler('규제').crawl_news('ukraine regulation', 2).write_json('en_RegulationNewsData')
@@ -50,7 +51,8 @@ except Exception as e:
 try:
     GoogleCrawler('후원').crawl_news('우크라이나 후원', 2).write_json('kr_SponsorNewsData')
     print('kr_SponsorNewsData')
-except:
+except Exception as e:
+    print(e)
     print('kr_SponsorNewsData error')
 
 # try:
@@ -62,7 +64,8 @@ except:
 try:
     GoogleCrawler('협상 진행 과정').crawl_news('우크라이나 협상 진행 상황', 2).write_json('kr_NegoNewsData')
     print('kr_NegoNewsData')
-except:
+except Exception as e:
+    print(e)
     print('kr_NegoNewsData error')
 
 # try:
@@ -74,7 +77,8 @@ except:
 try:
     GoogleCrawler('지역 라이브 상황').crawl_news('우크라이나 실시간 상황', 2).write_json('kr_LocalNewsData')
     print('kr_LocalNewsData')
-except:
+except Exception as e:
+    print(e)
     print('kr_LocalNewsData error')
 
 # try:
@@ -86,5 +90,6 @@ except:
 try:
     GoogleCrawler('지역 라이브 상황').crawl_news('우크라이나 피해 상황', 2).write_json('kr_DamageNewsData')
     print('kr_DamageNewsData')
-except:
+except Exception as e:
+    print(e)
     print('kr_DamageNewsData error')
